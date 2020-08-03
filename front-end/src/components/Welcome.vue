3eed2bfa5db6bd24311954915b5a545d43f69f2a
Cis Cuypers <ciscuypers@gmail.com>
2020-08-03 17:00:09 +0200

    create html for welcome page

--- front-end/src/components/Welcome.vue
+++ front-end/src/components/Welcome.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="hello">
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name">
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Welcome',
   data () {
     return {
+      name: null,
     }
   }
 }

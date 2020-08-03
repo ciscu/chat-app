24a4a35b913cde20629f76bebd07b2af6eda300c
Cis Cuypers <ciscuypers@gmail.com>
2020-08-03 18:27:42 +0200

    add html structure to newMessage component

--- front-end/src/components/NewMessage.vue
+++ front-end/src/components/NewMessage.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="">
+  <div class="new-message">
+    <form @submit.prevent="addMessage">
+      <label for="new-message">New Message (enter to add):  </label>
+      <input type="text" name="new-message" v-model="newMessage">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'NewMessage',
+  props: ['name'],
   data () {
     return {
+      newMessage: null,
+    }
+  },
+  methods: {
+    addMessage(){
+      console.log(this.newMessage, this.name, Date.now())
     }
   }
 }

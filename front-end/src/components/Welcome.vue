8d8aff4966bc5ff2dbcf9e6004cc63857ca8a46a
Cis Cuypers <ciscuypers@gmail.com>
2020-08-03 17:07:00 +0200

    add style to welcome component

--- front-end/src/components/Welcome.vue
+++ front-end/src/components/Welcome.vue
@@ -31,4 +31,15 @@ export default {
 </script>
 
 <style>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
+
+}
+.welcome h2 {
+  font-size: 3em;
+}
+.welcome button {
+  margin: 30px auto;
+}
 </style>
